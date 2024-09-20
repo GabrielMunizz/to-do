168d7da1edf3d412e710321dd4285c1c6b9e2941
Muniz <gabrielmfd@gmail.com>
2024-09-20 07:02:14 -0300

    Feat: Cria e implementa context e provider para a aplicação

---
+++ src/context/ToDoContext.tsx
@@ -0,0 +1,34 @@
+import { TaskType } from "@/utils/types";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
+
+type ToDoContextType = {
+  isModalOpen: boolean;
+  setIsModalOpen: Dispatch<SetStateAction<boolean>>;
+  tasks: TaskType[];
+  setTasks: Dispatch<SetStateAction<TaskType[]>>;
+  saveTask: (newTask: string) => void;
+};
+
+export const TodoContext = createContext({} as ToDoContextType);
+
+type TodoProviderType = {
+  children: React.ReactNode;
+};
+
+export const TodoProvider = ({ children }: TodoProviderType) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [tasks, setTasks] = useState<TaskType[]>([]);
+
+  const saveTask = (newTask: string) => {
+    const addNewTask = [...tasks, { id: tasks.length + 1, toDo: newTask }];
+    setTasks(addNewTask);
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{ saveTask, isModalOpen, setIsModalOpen, tasks, setTasks }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
